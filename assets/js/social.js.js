4341f998a405a01509e2cebdbc6efe91a869d1e1
David Lucas Alves da Silva <davidlucas@SGEX.EB.MIL.BR>
2026-03-24 11:23:48 -0300

    first commit

---
+++ assets/js/social.js.js
@@ -0,0 +1,27 @@
+const social = [...document.querySelectorAll(".social")];
+const sociais = [
+    'https://www.linkedin.com/in/lucianolpsf/',
+    'https://github.com/lucianolpsf',
+    'https://facebook.com/lucianolpsf',
+    'https://instagram.com/lucianolpsf',
+    'https://www.youtube.com/@proletariovencedor?sub_confirmation=1'];
+
+const sociais_name = ["linkedin-in","github","facebook","instagram","youtube"];
+
+social.map((element)=>{
+
+    sociais.map((elemento, indice)=>{
+        const link = document.createElement("a");
+              link.setAttribute("href",elemento);
+              link.setAttribute("target","_blank");
+              link.setAttribute("aria-label",sociais_name[indice])
+        
+        const icons = document.createElement("i");
+              icons.setAttribute("class","fa-brands fa-"+sociais_name[indice])
+
+        
+        link.appendChild(icons);
+        element.appendChild(link);
+    })
+    
+})
